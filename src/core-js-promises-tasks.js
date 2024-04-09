c69e7914024323145512451aaf4fc08cd0779af1
Roman Gromov <gromov.r.v.92@gmail.com>
2024-04-09 23:29:01 +0500

    feat: queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -129,8 +129,14 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce(
+    (chain, current) =>
+      chain.then((result) =>
+        current.then((currentResult) => `${result}${currentResult}`)
+      ),
+    Promise.resolve('')
+  );
 }
 
 module.exports = {
